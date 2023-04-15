bbc331fdea8127186bb8c85c04e4fc57943f9b21
eqfwcev123 <dohyeonee95@hotmail.com>
2023-04-15 23:02:15 +0800

    Refactored app.py

--- app.py
+++ app.py
@@ -1,64 +1,56 @@
 import ccxt
-import telegram
+from module.Telegram import telegram
 import talib
 import numpy as np
 import requests
 import time
-import config
 
 from datetime import datetime
+from module.Strategy import strategy as Strategy
+from module.Exchange import exchange as Exchange
 
-token = config.TOKEN
-chat_id = config.CHAT_ID
-bot = telegram.Bot(token=token)
+Exchange.add_symbols(
+    [{
+        "ticker": "BTC/USDT:USDT",
+        "timeframe": "5m",
+    },
+    {
+        "ticker": "ETH/USDT:USDT",
+        "timeframe": "5m",
+    },
+    {
+        "ticker": "XRP/USDT:USDT",
+        "timeframe": "5m",
+    }]
+)
+latest_timestamps = np.zeros_like(Exchange.symbols)
 
-exchange = ccxt.bitget({
-    "apiKey": config.API_KEY,
-    "secret": config.SECRET_KEY,
-    "password": config.PASSWORD
-})
-
-balance = exchange.fetch_balance()
-
-if len(balance['total']) == 0:
-    print('Failed to fetch balance. Please check your API key and secret key.')
-else:
-    print("Connected to Bitget")
-
-symbol = "ETH/USDT"
-timeframe = "5m"
-limit=1000
-latest_timestamp = 0
-
-# ohlcv => open, high , low, close, volume
 
 while True:
     # Fetch latest dataset
-    latest_ohlcv = exchange.fetch_ohlcv(symbol, timeframe, limit=1)
-    # timestamp = datetime.fromtimestamp(latest_ohlcv[0][0]/1000)
-    latest_ohlcv_timestamp = latest_ohlcv[0][0]
+    latest_ohlcvs = Exchange.get_latest_ohlcvs()
 
-    # Check if the latest timestamp is already in the data
-    if latest_ohlcv_timestamp > latest_timestamp:
-        latest_timestamp = latest_ohlcv_timestamp
+    for index, latest_ohlcv in enumerate(latest_ohlcvs):
+        ohlcv_timestamp = latest_ohlcv[0]
+        ohlcv_open = latest_ohlcv[1]
+        ohlcv_high = latest_ohlcv[2]
+        ohlcv_low = latest_ohlcv[3]
+        ohlcv_close = latest_ohlcv[4]
+
+        # Check if the latest timestamp is already in the data
+        if ohlcv_timestamp > latest_timestamps[index]:
+            latest_timestamps[index] = ohlcv_timestamp
 
         # Fetch full dataset
-        ohlcv_data = exchange.fetch_ohlcv(symbol, timeframe, limit=limit)
+        ohlcv_data = Exchange.get_history_ohlcvs(index)
+        history_data_close_prices = np.array([ohlcv[4] for  ohlcv in ohlcv_data])
 
-        # Calculate the 25 EMA, 50 EMA, and 100 EMA
-        close_prices = np.array([ohlcv[4] for ohlcv in ohlcv_data])
-        ema_25 = talib.EMA(close_prices, timeperiod=25)[-1]
-        ema_50 = talib.EMA(close_prices, timeperiod=50)[-1]
-        ema_100 = talib.EMA(close_prices, timeperiod=100)[-1]
-
-        # Check if current market price is touching 25 EMA
-        current_price = latest_ohlcv[0][4]
-        if current_price >= ema_25:
-            # Send a signal using Telegram
-            message = f'Current market price ({current_price}) is touching the 25 EMA ({ema_25}) on the {timeframe} chart for {symbol}'
-            requests.get(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}')
-        else:
-            requests.get(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text="Test"')
+        Strategy.EMA25Strategy(
+            ohlcv_close, 
+            history_data_close_prices, 
+            Exchange.symbols[index]["timeframe"],
+            Exchange.symbols[index]["ticker"]
+            )
 
     time.sleep(1)
 
